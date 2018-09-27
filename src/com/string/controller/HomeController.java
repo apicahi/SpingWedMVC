7348d68b86bd05c23a7181f441202d94ddfd97e9
BeerLove36 <apichai.som@msu.ac.th>
2018-09-27 10:50:40 +0700

    commet

--- src/com/string/controller/HomeController.java
+++ src/com/string/controller/HomeController.java
@@ -1,6 +1,6 @@
-package main;
+package com.string.controller;
 
-public class Main {
+public class HomeController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
